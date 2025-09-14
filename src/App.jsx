2003e6c20e3d48400c30ce89aaee74361252e42e
Khaled Ansar <khaledansar.in@gmail.com>
2025-09-14 15:54:08 +0100

    feat(movies): implement movie fetching with TMDB API and add Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Component/Search.jsx";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    try {
+      const endPoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const res = await fetch(endPoint, API_OPTIONS);
+
+      if (!res.ok) {
+        throw new Error(`Failed to fetch movies`);
+      }
+      const data = await res.json();
+
+      if (!data.Response === false) {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovies([]);
+        return;
+      }
+      setMovies(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage(`Error fetching movies : ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
